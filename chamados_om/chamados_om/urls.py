8f33061518330f5ac1a47ebbfdd077f7bdc728d7
Douglas Willan <douglaswillan7@gmail.com>
2017-10-08 20:11:19 -0300

    Arquivos iniciais

---
+++ chamados_om/chamados_om/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'chamados_om.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
