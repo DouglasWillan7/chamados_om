de7cd9c09ab50f59e84b96bae21c596d630cc4ab
Douglas Willan <douglaswillan7@gmail.com>
2017-10-08 22:28:21 -0300

    validação de formulario, rotas, atualização do banco

--- chamados_om/perfis/models.py
+++ chamados_om/perfis/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Perfil(models.Model):
 
     nome = models.CharField(max_length=255, null=False)
-    email = models.CharField(max_length=255, null=False)
     telefone = models.CharField(max_length=15, null=False)
+    usuario = models.OneToOneField(User, related_name="perfil")
 
+    @property
+    def email(self):
+        return self.usuario.email
 
     def convidar(self, perfil_convidado):
         pass
