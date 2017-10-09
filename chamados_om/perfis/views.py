a694c8d1d26bff067f2f6cbc5036d991e1747407
Douglas Willan <douglaswillan7@gmail.com>
2017-10-08 22:36:29 -0300

    login, decorator para funções internas

--- chamados_om/perfis/views.py
+++ chamados_om/perfis/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
 from perfis.models import Perfil
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def index(request):
-    return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
+    return render(request, 'index.html', {'perfis' : Perfil.objects.all(), 'perfil_logado': get_perfil_logado(request)})
 
+@login_required
 def exibir(request, perfil_id):
 
     perfil = Perfil.objects.get(id=perfil_id)
     return render(request, 'perfil.html', { "perfil" : perfil})
+
+@login_required
+def get_perfil_logado(request):
+    return request.user.perfil
